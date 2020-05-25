6f30c165c63825c141116113a3c7b1a2594f943f
Maribelle Leong <maribelle.leong@gmail.com>
2020-05-25 19:26:32 +0000

    interpolate strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual !== expected)
-    console.log("💥️💥️💥️ Assertion failed: " + `${actual} !== ${expected}`);
-  else
-    console.log("✅️✅️✅️ Assertion Passed: " + `${actual} === ${expected}`);
+    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
+  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
 };
 
 // TEST CODE
