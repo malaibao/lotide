9a4d67836ddda91b8cd86a2b298b822ae9d83a5d
Maribelle Leong <maribelle.leong@gmail.com>
2020-05-27 01:28:15 +0000

    implemented countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const countLetters = (input) => {
+  let str = input.split(" ").join("").toLowerCase();
+  let countObj = {};
+  for (let char of str) {
+    if (!(char in countObj))
+      // if not exists
+      countObj[char] = 1;
+    else countObj[char]++;
+  }
+  console.log(countObj);
+};
+
+// assertEqual Func
+const assertEqual = function (actual, expected) {
+  if (actual !== expected)
+    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
+  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+};
+
+countLetters("Hello Hello");
+countLetters("lighthouse in the house");
