e3122f229c3bf2034dad352035598a41a8aa053b
Maribelle Leong <maribelle.leong@gmail.com>
2020-05-27 02:14:10 +0000

    implemented findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const findKeyByValue = (inputObj, target) => {
+  let foundKey;
+  for (let key of Object.keys(inputObj)) {
+    if (inputObj[key] === target) foundKey = key;
+  }
+  return foundKey;
+};
+
+// assertEqual Func
+const assertEqual = function (actual, expected) {
+  if (actual !== expected)
+    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
+  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+};
+
+// TEST CASE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
