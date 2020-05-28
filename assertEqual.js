29f1961417fff341f48abb82e9b18257a9a225aa
Maribelle Leong <maribelle.leong@gmail.com>
2020-05-28 14:47:29 +0000

    fixed if statement with curly brackets

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
-  if (actual !== expected)
+  if (actual !== expected) {
     console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
-  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  }
 };
 
 // TEST CODE
