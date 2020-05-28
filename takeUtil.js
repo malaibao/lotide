6efb49f82a516895830fc037e8a560835f0a9d61
Maribelle Leong <maribelle.leong@gmail.com>
2020-05-28 14:37:35 +0000

    implemented takeUtil

---
+++ takeUtil.js
@@ -0,0 +1,46 @@
+const takeUntil = function (array, callback) {
+  const result = [];
+  for (item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+
+// eqArray FUCTION
+const eqArrays = (a, b) => {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+
+// assertArraysEqual FUCTION
+const assertArraysEqual = (a, b) => {
+  let isEqual = eqArrays(a, b);
+
+  isEqual
+    ? console.log(`✅️✅️✅️ Assertion passed: ${a} === ${b}`)
+    : console.log(`💥️💥️💥️ Assertion failed: ${a} !== ${b}`);
+};
+const results1 = takeUntil(data1, (x) => x < 0);
+
+const results2 = takeUntil(data2, (x) => x === ",");
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
