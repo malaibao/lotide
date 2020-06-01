fe8e64e0a30776b0ba8484105f6104b82b796d64
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 18:15:52 +0000

    refactored by exporting test case to test file

--- countOnly.js
+++ countOnly.js
@@ -18,36 +18,14 @@ const countOnly = function (allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
-
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-});
-
 /*
-simpler way
-if (itemsToCount[item]) { 
-  if (results[item]) {
-    results[item] += 1;
-  } else {
-    results[item] = 1;
+  simpler way
+  if (itemsToCount[item]) { 
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
-}
-*/
-
-// TEST CODE
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
+  */
+module.exports = countOnly;
