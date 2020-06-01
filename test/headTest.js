eeca4e7e94785c1db7d6e80652f4b7bae292bbc8
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 16:29:41 +0000

    apply gitignore

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+
+const assert = require("chai").assert;
 const head = require("../head");
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// console.log(head([]));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 6 for [6,7,8]", () => {
+    assert.strictEqual(head([6, 7, 8]), 6);
+  });
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
