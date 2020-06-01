6cba6464189adeac68e848f165714c49f1124e1c
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 18:38:51 +0000

    moved test cases to mocha test folder

--- countLetters.js
+++ countLetters.js
@@ -1,23 +1,18 @@
-const countLetters = (input) => {
+// const assertObjectsEqual = require("./assertObjectsEqual");
+
+const countLetters = input => {
   let str = input.split(" ").join("").toLowerCase();
   let countObj = {};
   for (let char of str) {
+    // if not exists
     if (!(char in countObj)) {
-      // if not exists
       countObj[char] = 1;
     } else {
       countObj[char]++;
     }
   }
-  console.log(countObj);
+  // console.log(countObj);
+  return countObj;
 };
 
-// assertEqual Func
-const assertEqual = function (actual, expected) {
-  if (actual !== expected)
-    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
-  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
-};
-
-countLetters("Hello Hello");
-countLetters("lighthouse in the house");
+module.exports = countLetters;
