2f35331ff19160b3673ce71bdb23045c040542f9
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 16:50:00 +0000

    modified test case with mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -17,10 +17,3 @@ describe("#middle", () => {
     expect(middle([1, 2, 3, 4, 5, 6])).to.deep.equal([3, 4]);
   });
 });
-
-// ssertArraysEqual(middle([1]), []); // => []
-// assertArraysEqual(middle([1, 2]), []); // => []
-// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
