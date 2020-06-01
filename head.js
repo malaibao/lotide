39362623c4c5df00e71a29ab86393d5d7d56404a
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 14:53:06 +0000

    created test folder and implemented head test in separate file

--- head.js
+++ head.js
@@ -1,20 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual !== expected)
-    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
-  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
-};
-
 const head = function (array) {
   return array !== null ? array[0] : undefined;
-}
+};
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([]));
+module.exports = head;
