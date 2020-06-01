63cca3e3859e9c2813ea28dc1f10dc323719552d
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 19:32:49 +0000

    removed eqObject1 old version and refactored eqObject2 and changed to eqObject1

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,5 @@
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
@@ -8,42 +10,18 @@ const eqObjects = function (object1, object2) {
     return false;
   }
 
-  // for (let [key,value] of Object.entries(object1))
-
-  for (let key of Object.keys(object1)) {
-    // Check if value is an array
-    if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key], object2[key]);
+  for (let [key, value] of Object.entries(object1)) {
+    // check object type value
+    if (typeof value === "object") {
+      return eqObjects(value, object2[key]);
+    }
+    // Check array type value
+    if (Array.isArray(value)) {
+      return eqArrays(value, object2[key]);
     }
 
     // Check primitive type value
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// assertEqual Func
-const assertEqual = function (actual, expected) {
-  if (actual !== expected)
-    console.log(`💥️💥️💥️ Assertion failed: ${actual} !== ${expected}`);
-  else console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
-};
-
-// eqArray FUCTION
-const eqArrays = (a, b) => {
-  // Check if both are array
-  if (!Array.isArray(a) || !Array.isArray(b)) {
-    return false;
-  }
-  // Check array length
-  if (a.length !== b.length) {
-    return false;
-  }
-
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
+    if (value !== object2[key]) {
       return false;
     }
   }
@@ -51,36 +29,23 @@ const eqArrays = (a, b) => {
 };
 
 //TEST CASES
+// Check nested object
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
 
-// Check same object
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
 
-//Check different keys length
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
 
-// Check different order with primitive
-const xy = { a: "1", b: "1" };
-const yx = { b: "2", a: "1" };
-console.log(eqObjects(xy, yx)); // => false
+console.log(
+  eqObjects(
+    { a: { z: 1, a: { b: 1 } }, b: 2 },
+    { a: { z: 1, a: { b: 1 } }, b: 2 }
+  )
+); // => true
 
-// Check different keys
-const x1 = { a: "1", b: "1" };
-const x2 = { a: "1", c: "1" };
-console.log(eqObjects(xy, yx)); // => false
-
-// Check different key order with array
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-// Check different arrays
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
-
-// Check one primitive , one array, SAME OBJECTS
-const a1 = { c: "1", d: ["2", 3, 4] };
-const a2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(a1, a2)); // => true
+console.log(
+  eqObjects(
+    { a: { y: 0, z: 1 }, b: 2, c: [1, 2] },
+    { a: { y: 0, z: 1 }, b: 2, c: [1, 2] }
+  ) // => true
+);
