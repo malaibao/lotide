64a2eecba043ec2b17e6357272ff212bd4ebb8ed
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-01 16:49:17 +0000

    modified test case with mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,26 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+// const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+  it("return [] for [1]", () => {
+    expect(middle([1])).to.deep.equal([]);
+  });
+  it("return [] for [1,2]", () => {
+    expect(middle([1, 2])).to.deep.equal([]);
+  });
+  it("return [3] for [1,2,3,4,5]", () => {
+    expect(middle([1, 2, 3, 4, 5])).to.deep.equal([3]);
+  });
+  it("return [3,4] for [1,2,3,4,,6]", () => {
+    expect(middle([1, 2, 3, 4, 5, 6])).to.deep.equal([3, 4]);
+  });
+});
+
+// ssertArraysEqual(middle([1]), []); // => []
+// assertArraysEqual(middle([1, 2]), []); // => []
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
